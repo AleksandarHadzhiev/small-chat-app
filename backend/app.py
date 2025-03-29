133f11ff8b1612b7e568ed2281ec52ed28a549c3
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-03-29 10:58:29 +0100

    backend ref to handle multiple groups properly

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,55 @@
 import json
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from fastapi.middleware.cors import CORSMiddleware
 # Registered users and their websockets
 users = {}
 
-app = FastAPI()
-origins = [
-    "*"
-]
+class Group:
+    def __init__(self, name, email, type):
+        self.name = name
+        self.type = type
+        self.members = []
+        self.members.append(email)
+        self.messages = []
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods="*",
-    allow_headers="*"
-)
+
+    async def send_message_to_members(self, content, email, connections):
+        for member in self.members:
+            ws: WebSocket = connections[member]
+            content = f"{email}: {content}"
+            print(content)
+            message = {
+                "content": content,
+                "group": self.name
+            }
+            self.messages.append(content)
+            await ws.send_json(message)
+
+    def update_name(self, name, owner):
+        if owner == self.members[0]:
+            self.name = name
+
+
+    def group_join(self, email):
+        self.members.append(email)
+
+
+    def leave_group(self, email):
+        if email in self.members:
+            self.members.pop(email)
+
+
+    def remove_user(self, email, owner):
+        if owner == self.members[0]:
+            self.leave_group(email=email)
+
+
+groups: list[Group] = []
+
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections = users
+        self.active_connections = {}
 
 
     async def connect(self, ws: WebSocket, email: str):
@@ -31,12 +61,32 @@ class ConnectionManager:
         self.active_connections.pop(email)
 
 
-    async def broadcast(self, message: str):
-        for email in self.active_connections:
-            ws: WebSocket = self.active_connections[email]
-            await ws.send_text(message)
+    async def broadcast(self, message, email):
+        content = message["content"]
+        group_name = message["group"]
+        if len(groups) > 0:
+            for group in groups:
+                if group.name == group_name:
+                    await group.send_message_to_members(content=content, email=email, connections=self.active_connections)
+
+
+app = FastAPI()
+origins = [
+    "*"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods="*",
+    allow_headers="*"
+)
+
 
 connection_manager = ConnectionManager()
+
 
 @app.get("/")
 def root():
@@ -78,8 +128,98 @@ async def websocket_endpoint(websocket: WebSocket, email: str):
     await connection_manager.connect(ws=websocket, email=email)
     try:
         while True:
-            data = await websocket.receive_text() 
-            await connection_manager.broadcast(f"Client #{email}: {data}")
+            data = await websocket.receive_json() 
+            await connection_manager.broadcast(data, email)
     except WebSocketDisconnect:
         connection_manager.disconnect(email=email)
         await connection_manager.broadcast(f"Client #{email} left the chat")
+
+
+@app.get("/groups")
+async def get_groups():
+    _groups = []
+    for group in groups:
+        _groups.append(group.name)
+    print(_groups)
+    return {"groups": _groups}
+
+
+@app.post("/")
+async def create_group(request: Request):
+    body = await request.json()
+    if "name" not in body:
+        return {"fail": "A group needs a name to be created."}
+    elif "email" not in body:
+        return {"fail": "A user is needed to create a group."}
+    # Continue proper checking
+    name = body["name"]
+    email = body["email"]
+    type = body["type"]
+    group = Group(name=name, email=email, type=type)
+    groups.append(group)
+    return {"message": "A group is created"}
+
+
+# JOIN GROUP
+@app.post("/group/join")
+async def join_group(request: Request):
+    body = await request.json()
+    group_name = body["name"]
+    user = body["email"]
+    for group in groups:
+        if group.name == group_name:
+            group.group_join(email=user)
+
+
+# LEAVE GROUP
+@app.post("/leave")
+async def leave_group(request: Request):
+    body = await request.json()
+    email = body["email"]
+    group_name = body["name"]
+    for group in groups:
+        if group.name == group_name:
+            group.leave_group(email=email)
+
+
+# EDIT GROUP
+@app.put("/edit")
+async def update_name(request: Request):
+    body = await request.json()
+    owner = body["owner"]
+    group_name = body["name"]
+    new_name = body["newName"]
+    for group in groups:
+        if group.name == group_name:
+            group.update_name(name=new_name, owner=owner)
+
+
+# REMOVE MEMBER FROM GROUP
+@app.post("/remove")
+async def remove_member_from_group(request: Request):
+    body = await request.json()
+    email = body["email"]
+    owner = body["owner"]
+    group_name = body["name"]
+    for group in groups:
+        if group.name == group_name:
+            group.remove_user(email=email, owner=owner)
+
+
+# DELETE GROUP
+@app.delete("/delete")
+async def delete_group(request: Request):
+    body = await request.json()
+    owner = body["owner"]
+    group_name = body["name"]
+    for group in groups:
+        if group.name == group_name and group[0] == owner:
+            groups.pop(group)
+
+
+@app.get("/{group_name}/messages")
+async def getMessages(group_name):
+    for group in groups:
+        if group.name == group_name:
+            return {"messages": group.messages}
+    return {"messages": []}
