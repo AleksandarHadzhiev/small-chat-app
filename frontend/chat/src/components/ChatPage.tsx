c1cc329541d28afeba9924928502c9f1210a3286
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-03-30 10:43:04 +0200

    ref frontend for better message handling

--- frontend/chat/src/components/ChatPage.tsx
+++ frontend/chat/src/components/ChatPage.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { FormEvent, MouseEvent, useState } from "react";
+import { FocusEvent, FormEvent, MouseEvent, useState } from "react";
 
 //@ts-ignore
 export default function Chat({ groupName }) {
@@ -33,28 +33,53 @@ export default function Chat({ groupName }) {
 
     function showMessage(message: string) {
         const data = JSON.parse(message)
-        if (data.group === groupName) {
+        if (data.group === groupName && data.tag === "send message") {
             const messageContainer = document.getElementById("messages")?.lastElementChild;
-            console.log(document.getElementById("messages"))
             const messageElement = document.createElement('p');
             messageElement.textContent = data.content
             messageContainer?.append(messageElement)
+        }
+        else if (data.tag == "typing") {
+            const messageContainer = document.getElementById("send-message")
+            if (messageContainer) {
+                const typingElement = document.createElement('div')
+                typingElement.className = "w-full bg-gray-400 text-white"
+                typingElement.textContent = data.email + ' is typing ...'
+                typingElement.setAttribute("id", "typing")
+                const last = messageContainer.lastChild
+                if (last)
+                    messageContainer.insertBefore(typingElement, last)
+                else
+                    messageContainer.appendChild(typingElement)
+            }
+        }
+        else if (data.tag == "stopped typing") {
+            const messageContainer = document.getElementById("typing")
+            if (messageContainer)
+                messageContainer.remove()
+
         }
     }
 
     function sendMessage(e: FormEvent<HTMLFormElement>) {
         e.preventDefault()
-        socket.send(JSON.stringify({ content: message, group: groupName }))
+        const content = {
+            message: message,
+            type: {
+                title: 'message',
+                tag: 'send message'
+            }
+        }
+        setMessage("")
+        socket.send(JSON.stringify({ content: content, group: groupName }))
     }
 
     function join(e: any) {
         e.preventDefault()
-        console.log(groupName)
         const content = {
             name: groupName,
             email: localStorage.getItem("user"),
         }
-        console.log(content)
         fetch("http://127.0.0.1:8000/group/join", { method: "POST", body: JSON.stringify(content) })
             .then(async (res) => {
                 if (res.status == 200) {
@@ -69,6 +94,27 @@ export default function Chat({ groupName }) {
             })
     }
 
+    function displayTyping(e: FocusEvent<HTMLInputElement, Element>) {
+        e.preventDefault()
+        const notification = {
+            type: {
+                title: 'notification',
+                tag: 'typing'
+            }
+        }
+        socket.send(JSON.stringify({ content: notification, group: groupName }))
+    }
+
+    function removeTyping(e: FocusEvent<HTMLInputElement, Element>) {
+        e.preventDefault()
+        const notification = {
+            type: {
+                title: 'notification',
+                tag: 'stopped typing'
+            }
+        }
+        socket.send(JSON.stringify({ content: notification, group: groupName }))
+    }
 
     return (
         <div className="flex flex-col w-3/4 h-full">
@@ -84,18 +130,21 @@ export default function Chat({ groupName }) {
                     </div>
                     <div id="messages" className="w-full h-8/10 bg-black text-gray-200 overflow-auto">
                     </div>
-                    <form onSubmit={(e) => { sendMessage(e) }} className="flex w-full h-1/10 bg-gray-900 text-white">
+                    <form id="send-message" onSubmit={(e) => { sendMessage(e) }} className="flex flex-col w-full h-1/10 bg-gray-900 text-white">
                         <div className="flex w-full h-full">
                             <input
+                                onFocus={(e) => { displayTyping(e) }}
+                                onBlur={(e) => { removeTyping(e) }}
                                 className="w-full h-full p-2"
                                 type="text"
+                                value={message}
                                 placeholder="Type..."
                                 onChange={(e) => { setMessage(e.target.value) }} />
+                            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold  py-2 px-4 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                            </svg>
+                            </button>
                         </div>
-                        <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                        </svg>
-                        </button>
                     </form>
                 </div>
             ) : (
